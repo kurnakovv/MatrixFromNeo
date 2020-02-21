a5d0e2cbba494aa75e53e0016fb78378ca654307
maksk210@gmail.com <maksk210@gmail.com>
2020-02-21 18:04:21 +0900

    Добавьте файлы проекта.

---
+++ Messeges.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MatrixFromNeo
+{
+    public static class Messeges
+    {
+        public static void Print<T>(this IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item);
+                Thread.Sleep(300);
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.Clear();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MatrixFromNeo
+{
+    class Program
+    {
+        const int STD_OUTPUT_HANDLE = -11;
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetStdHandle(int handle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleDisplayMode(IntPtr ConsoleHandle, uint Flags, IntPtr NewScreenBufferDimensions);
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+
+            var hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleDisplayMode(hConsole, 1, IntPtr.Zero);
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+
+            var str1 = "Wake up, Neo...";
+            str1.Print();
+
+            var str2 = "The Matrix has you...";
+            str2.Print();
+
+            var str3 = "Follow the white rabbit.";
+            str3.Print();
+
+            var str4 = "Knock, knock, Neo.";
+            str4.Print();
+        }
+    }
+}
